cb7c958f7f0619a0cfb68632d1e76d108848a0bd
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-08 12:55:18 -0800

    minor additions

---
+++ Ch2/OOP.py
@@ -0,0 +1,16 @@
+#What is OOP?
+#OOP is a programmin convention, similar to functional programming, 
+#but uses objects as actors
+#Each object is an instance of a class. 
+
+
+#Why progrram this way? 
+#There are 3 reasons to implement oop
+#			- Robustness
+#			- Adaptability 
+#			- Reusability 
+
+#OOD design principles 
+#			- Modularity
+#			- Abstraction 
+#			- Encapulation 
